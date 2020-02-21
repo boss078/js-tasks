78b16f441508d08ceb8a91d36b3d409af3ca2802
boss078 <JohnUnknownSmith@mail.ru>
2020-02-22 00:14:20 +0300

    Formatted code according to Airbnb code style

--- files/main-script.js
+++ files/main-script.js
@@ -1,168 +1,158 @@
-$(document).ready(function() {
-	const getPictureFromArray = function getPictureNameFromArrayWithId(line, column) {
-		let elemHtml = "";
-		switch(arguments.length) { 
-			case 1:
-				elemHtml = $(line).html();
-			break;
-			case 2:
-				elemHtml = $("#" + line.toString() + column.toString()).html();
-			break;
-		}
-		let answ = "";
-		for (let i = 14; elemHtml[i] != '.'; i += 1)
-			answ += elemHtml[i];
-		return answ;
-	}
-	const setPictureFromArray = function setPictureNameFromArrayWithId(line, column, value) {
-		switch(arguments.length) {
-			case 2:
-				$(line).html("<img src=\"img/" + column.toString() + ".png\"> ");
-			break;
-			case 3:
-				$("#" + line.toString() + column.toString()).html("<img src=\"img/" + value.toString() + ".png\"> ");
-			break;
-		}
-	}
+$(document).ready(() => {
+  let isPlaying;
+  let currPlayer;
 
-	const checkWin = function CheckIfWonOfPlayersWin() {
-		let currSymbol;
-		let isWon;
-		for(let column = 0; column < 3; column += 1) {
-			currSymbol = ""
-			isWon = true;
-			for(let line = 0; line < 3; line += 1) {
-				if(getPictureFromArray(line, column) === "Blank") {
-					isWon = false;
-					break;
-				}
-				if (currSymbol === "") {
-					currSymbol = getPictureFromArray(line, column);
-				} else if(currSymbol != getPictureFromArray(line, column)){
-					isWon = false;
-					break;
-				}	
-			}
-			if(isWon === true) {
-				if(currSymbol === "X")
-					return 1;
-				else
-					return 2;
-			}
-		}
-		for(let line = 0; line < 3; line += 1) {
-			currSymbol = ""
-			isWon = true;
-			for(let column = 0; column < 3; column += 1) {
-				if(getPictureFromArray(line, column) === "Blank") {
-					isWon = false;
-					break;
-				}
-				if (currSymbol === "") {
-					currSymbol = getPictureFromArray(line, column);
-				} else if(currSymbol != getPictureFromArray(line, column)){
-					isWon = false;
-					break;
-				}	
-			}
-			if(isWon === true) {
-				if(currSymbol === "X")
-					return 1;
-				else
-					return 2;
-			}
-		}
-		currSymbol = ""
-		isWon = true;
-		for(let line = 0, column = 0; line < 3; line += 1, column += 1) {
-			if(getPictureFromArray(line, column) === "Blank") {
-					isWon = false;
-					break;
-				}
-			if (currSymbol === "") {
-				currSymbol = getPictureFromArray(line, column);
-			} else if(currSymbol != getPictureFromArray(line, column)){
-				isWon = false;
-				break;
-			}			
-		}
-		if(isWon === true) {
-			if(currSymbol === "X")
-				return 1;
-			else
-				return 2;
-		}
-		currSymbol = ""
-		isWon = true;
-		for(let line = 0, column = 2; line < 3; line += 1, column -= 1) {
-			if(getPictureFromArray(line, column) === "Blank") {
-					isWon = false;
-					break;
-				}
-			if (currSymbol === "") {
-				currSymbol = getPictureFromArray(line, column);
-			} else if(currSymbol != getPictureFromArray(line, column)){
-				isWon = false;
-				break;
-			}			
-		}
-		if(isWon === true) {
-			if(currSymbol === "X")
-				return 1;
-			else
-				return 2;
-		}
-		return 0;
-	};
+  const getPictureFromArray = function getPictureNameFromArrayWithId(line, column) {
+    let elemHtml = '';
+    switch (arguments.length) {
+      case 1:
+        elemHtml = $(line).html();
+        break;
+      case 2:
+        elemHtml = $(`#${line.toString()}${column.toString()}`).html();
+        break;
+      default:
+        console.log('Wrong use of getPictureFromArray');
+    }
+    let answ = '';
+    for (let i = 14; elemHtml[i] !== '.'; i += 1) answ += elemHtml[i];
+    return answ;
+  };
 
-	const checkTie = function checkIfAllCellsAreFilled(){
-		let isTie = true;
-		for (let line = 0; line < 3; line += 1)
-			for (let column = 0; column < 3; column += 1)
-				if (getPictureFromArray(line, column) === "Blank")
-					isTie = false;
-		if (isTie === true)
-			return true;
-		return false;
-	};
+  const setPictureFromArray = function setPictureNameFromArrayWithId(line, column, value) {
+    switch (arguments.length) {
+      case 2:
+        $(line).html(`<img src="img/${column.toString()}.png"> `);
+        break;
+      case 3:
+        $(`#${line.toString()}${column.toString()}`).html(`<img src="img/${value.toString()}.png"> `);
+        break;
+      default:
+        console.log('Wrong use of setPictureFromArray');
+    }
+  };
 
-	const init = function initializeAllValuesAndCells(){
-		isPlaying = true;
-		currPlayer = "X";
-		$(".hidden").hide();
-		$(".cell").html("<img src=\"img/Blank.png\"> ");
-	}
-	
-	let isPlaying;
-	let currPlayer;
-	init();
+  const checkWin = function CheckIfWonOfPlayersWin() {
+    let currSymbol;
+    let isWon;
+    for (let column = 0; column < 3; column += 1) {
+      currSymbol = '';
+      isWon = true;
+      for (let line = 0; line < 3; line += 1) {
+        if (getPictureFromArray(line, column) === 'Blank') {
+          isWon = false;
+          break;
+        }
+        if (currSymbol === '') {
+          currSymbol = getPictureFromArray(line, column);
+        } else if (currSymbol !== getPictureFromArray(line, column)) {
+          isWon = false;
+          break;
+        }
+      }
+      if (isWon === true) {
+        if (currSymbol === 'X') return 1;
+        return 2;
+      }
+    }
+    for (let line = 0; line < 3; line += 1) {
+      currSymbol = '';
+      isWon = true;
+      for (let column = 0; column < 3; column += 1) {
+        if (getPictureFromArray(line, column) === 'Blank') {
+          isWon = false;
+          break;
+        }
+        if (currSymbol === '') {
+          currSymbol = getPictureFromArray(line, column);
+        } else if (currSymbol !== getPictureFromArray(line, column)) {
+          isWon = false;
+          break;
+        }
+      }
+      if (isWon === true) {
+        if (currSymbol === 'X') return 1;
+        return 2;
+      }
+    }
+    currSymbol = '';
+    isWon = true;
+    for (let line = 0, column = 0; line < 3; line += 1, column += 1) {
+      if (getPictureFromArray(line, column) === 'Blank') {
+        isWon = false;
+        break;
+      }
+      if (currSymbol === '') {
+        currSymbol = getPictureFromArray(line, column);
+      } else if (currSymbol !== getPictureFromArray(line, column)) {
+        isWon = false;
+        break;
+      }
+    }
+    if (isWon === true) {
+      if (currSymbol === 'X') return 1;
+      return 2;
+    }
+    currSymbol = '';
+    isWon = true;
+    for (let line = 0, column = 2; line < 3; line += 1, column -= 1) {
+      if (getPictureFromArray(line, column) === 'Blank') {
+        isWon = false;
+        break;
+      }
+      if (currSymbol === '') {
+        currSymbol = getPictureFromArray(line, column);
+      } else if (currSymbol !== getPictureFromArray(line, column)) {
+        isWon = false;
+        break;
+      }
+    }
+    if (isWon === true) {
+      if (currSymbol === 'X') return 1;
+      return 2;
+    }
+    return 0;
+  };
 
-	$(".cell").click(function(){
-		if (isPlaying === true && getPictureFromArray(this) === "Blank") {
-			setPictureFromArray(this, currPlayer);
-			let winStatus = checkWin();
-			if(winStatus === 0) {
-				if(currPlayer === "X")
-					currPlayer = "O";
-				else
-					currPlayer = "X";
-				if (checkTie() === true) {
-					isPlaying = false;
-					$("div.hidden").show();
-					$("div.hidden").text("Tie!");
-					$("button.hidden").show();
-				}
-			}
-			else {
-				isPlaying = false;
-				$("div.hidden").show();
-				$("div.hidden").text("Player " + currPlayer + " win!");
-				$(".hidden").show();
-			}
-		}
-	});
+  const checkTie = function checkIfAllCellsAreFilled() {
+    let isTie = true;
+    for (let line = 0; line < 3; line += 1) for (let column = 0; column < 3; column += 1) if (getPictureFromArray(line, column) === 'Blank') isTie = false;
+    if (isTie === true) return true;
+    return false;
+  };
 
-	$("button.hidden").click(function(){
-		init();
-	});
+  const init = function initializeAllValuesAndCells() {
+    isPlaying = true;
+    currPlayer = 'X';
+    $('.hidable').hide();
+    $('.cell').html('<img src="img/Blank.png"> ');
+  };
+
+  $('.cell').click(function () {
+    if (isPlaying === true && getPictureFromArray(this) === 'Blank') {
+      setPictureFromArray(this, currPlayer);
+      const winStatus = checkWin();
+      if (winStatus === 0) {
+        if (currPlayer === 'X') currPlayer = 'O';
+        else currPlayer = 'X';
+        if (checkTie() === true) {
+          isPlaying = false;
+          $('div.hidable').show();
+          $('div.hidable').text('Tie!');
+          $('button.hidable').show();
+        }
+      } else {
+        isPlaying = false;
+        $('div.hidable').show();
+        $('div.hidable').text(`Player ${currPlayer} win!`);
+        $('.hidable').show();
+      }
+    }
+  });
+
+  $('button.hidable').click(() => {
+    init();
+  });
+
+  init();
 });
