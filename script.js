14ae6a9ebbf8a5037d79de5f2936594cf02e91e3
boss078 <izgachev01@mail.ru>
2020-03-26 00:21:10 +0300

    Connected eslint

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-  $(".choose-list").click(function() {
-    $(".choose-list").css("background-position", "0 -431px");
-    $(".choose-grid").css("background-position", "0 -327px");
-    $(".list-main__list-item").css("width", "100%");
-    $(".list-main__list-item").css("padding", "0");
-    $(".list-item__upper-block").css("padding", "30px 2.5% 15px");
-    $(".list-item__list-header").css("padding", "30px 2.5% 15px");
-    $(".list-item__info-block").css("padding", "30px 2.5% 15px");
+$(document).ready(() => {
+  $('.choose-list').click(() => {
+    $('.choose-list').css('background-position', '0 -431px');
+    $('.choose-grid').css('background-position', '0 -327px');
+    $('.list-main__list-item').css('width', '100%');
+    $('.list-main__list-item').css('padding', '0');
+    $('.list-item__upper-block').css('padding', '30px 2.5% 15px');
+    $('.list-item__list-header').css('padding', '30px 2.5% 15px');
+    $('.list-item__info-block').css('padding', '30px 2.5% 15px');
   });
-  $(".choose-grid").click(function() {
-    $(".choose-list").css("background-position", "0 -469px");
-    $(".choose-grid").css("background-position", "0 -289px");
-    $(".list-main__list-item").css("width", "20%");
-    $(".list-main__list-item").css("padding", "30px 2.5% 15px");
-    $(".list-item__upper-block").css("padding", "0");
-    $(".list-item__list-header").css("padding", "0");
-    $(".list-item__info-block").css("padding", "0");
+  $('.choose-grid').click(() => {
+    $('.choose-list').css('background-position', '0 -469px');
+    $('.choose-grid').css('background-position', '0 -289px');
+    $('.list-main__list-item').css('width', '20%');
+    $('.list-main__list-item').css('padding', '30px 2.5% 15px');
+    $('.list-item__upper-block').css('padding', '0');
+    $('.list-item__list-header').css('padding', '0');
+    $('.list-item__info-block').css('padding', '0');
   });
 });
